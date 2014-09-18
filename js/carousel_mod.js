ad66ff7c4251c1a0d0c310a92e44c227d2fff6cd
rvvind <arvind.suryakumar@gmail.com>
2014-09-17 20:42:55 -0500

    Initial Commit

---
+++ js/carousel_mod.js
@@ -0,0 +1,20 @@
+$('#myCarousel').carousel({
+  interval: false
+})
+
+$('.carousel[data-type="multi"] .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i=0;i<0;i++) {
+    next=next.next();
+    if (!next.length) {
+    	next = $(this).siblings(':first');
+  	}
+
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
